0e47d210c504fe9bcea3a71fd6ffd40a05ea2814
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-11-06 20:28:10 -0500

    added tags.js schema

--- server/models/tags.js
+++ server/models/tags.js
@@ -1,1 +1,17 @@
 // Tag Document Schema
+const mongoose = require('mongoose');
+
+const tagSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+module.exports = mongoose.model('Tag', tagSchema);
