c8b0881039fb1cb64b311b9be052673b14b0d49d
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-11-06 20:29:47 -0500

    added answer schema

--- server/models/answers.js
+++ server/models/answers.js
@@ -1,1 +1,19 @@
 // Answer Document Schema
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    ans_by: {
+        type: String,
+        default: 'Anonymous'
+    },
+    ans_date_time: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Answer', answerSchema);
