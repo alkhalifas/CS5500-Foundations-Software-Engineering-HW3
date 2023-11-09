074d063645c1f5c954dee1bd7d744ff76d9db8b8
Saleh Alkhalifa <alkhalifas@thallium.lan>
2023-11-08 20:26:00 -0500

    added tags with count to tags list page

--- server/server.js
+++ server/server.js
@@ -192,6 +192,41 @@ app.get('/questions/tag/:tagName', async (req, res) => {
 });
 
 /*
+Method that
+ */
+app.get('/tags-with-count', async (req, res) => {
+    try {
+        const tagCounts = await Question.aggregate([
+            {
+                $unwind: '$tags',
+            },
+            {
+                $group: {
+                    _id: '$tags',
+                    count: { $sum: 1 },
+                },
+            },
+        ]);
+
+        const tagsWithCount = await Promise.all(
+            tagCounts.map(async (tagCount) => {
+                const tag = await Tag.findById(tagCount._id);
+                return {
+                    _id: tagCount._id,
+                    name: tag ? tag.name : 'Unknown',
+                    count: tagCount.count,
+                };
+            })
+        );
+
+        res.json(tagsWithCount);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error getting tags with counts' });
+    }
+});
+
+/*
 Method that increments views by 1
  */
 app.post('/questions/increment-views/:questionId', async (req, res) => {
